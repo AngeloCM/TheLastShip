a0513d79554edf890180fc883d08ec076f5599b7
luke-goldfain <awsumluke22@gmail.com>
2019-12-02 23:36:51 -0600

    Fixed .gitignore, created project folders, created basic Player and Shot prefabs and scripts
    +GreenShot.mat
    +PlayerShip.prefab
    +PlayerShot.prefab
    +LukeControlsTest.unity
    +PlayerController.cs
    +BasicShot.cs

---
+++ TheLastShip/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField, Tooltip("The top speed at which the player can travel forward."), Range(5f, 1000f)]
+    public float TopSpeed = 100f;
+
+    [SerializeField, Tooltip("The speed at which the player's ship rotates."), Range(1f, 500f)]
+    public float RotationSpeed = 100f;
+
+    [SerializeField, Tooltip("The speed at which the player's ship accelerates."), Range(0.1f, 100f)]
+    public float AccelerationValue = 50f;
+
+    [SerializeField, Tooltip("The cooldown in seconds between basic shots."), Range(0.01f, 2f)]
+    public float ShotCooldown = 0.3f;
+
+    [SerializeField, Tooltip("The prefab for the player's basic projectile.")]
+    public GameObject PlayerShotPrefab;
+
+    [SerializeField, Tooltip("The transform at which to spawn a player shot.")]
+    public Transform PlayerShotTransform;
+
+    private Rigidbody rb;
+
+    private float yaw, pitch, roll;
+
+    private float currentSpeed;
+
+    private float shotTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+
+        shotTimer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RotateShip();
+
+        AccelerateShip();
+
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            shotTimer = 0f;
+        }
+    }
+
+    private void RotateShip()
+    {
+        yaw = Input.GetAxis("Horizontal") * Time.deltaTime * RotationSpeed;
+        pitch = Input.GetAxis("Vertical") * Time.deltaTime * RotationSpeed;
+        roll = -Input.GetAxis("Triggers") * Time.deltaTime * RotationSpeed;
+
+        this.transform.Rotate(new Vector3(0f, yaw, 0f));
+        this.transform.Rotate(new Vector3(pitch, 0f, 0f));
+        this.transform.Rotate(new Vector3(0f, 0f, roll));
+    }
+
+    private void AccelerateShip()
+    {
+        if (Input.GetButton("Throttle"))
+        {
+            if (currentSpeed < TopSpeed)
+            {
+                currentSpeed += AccelerationValue * Time.deltaTime;
+
+                if (currentSpeed > TopSpeed) currentSpeed = TopSpeed;
+            }
+        }
+
+        if (Input.GetButton("Brake"))
+        {
+            if (currentSpeed > 0f)
+            {
+                currentSpeed -= AccelerationValue * Time.deltaTime;
+
+                if (currentSpeed < 0f) currentSpeed = 0f;
+            }
+        }
+        
+        rb.velocity = transform.forward * currentSpeed;
+    }
+
+    private void Shoot()
+    {
+        if (shotTimer <= 0f)
+        {
+            Instantiate(PlayerShotPrefab, PlayerShotTransform);
+
+            shotTimer = ShotCooldown;
+        }
+
+        shotTimer -= Time.deltaTime;
+    }
+}
---
+++ TheLastShip/Assets/Scripts/Projectiles/BasicShot.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicShot : MonoBehaviour
+{
+    [SerializeField, Tooltip("The speed at which the shot travels. Ensure that this is greater than the player's TopSpeed."), Range(10f, 1000f)]
+    public float ShotSpeed = 200f;
+
+    [SerializeField, Tooltip("The time in seconds it takes for a shot to despawn automatically."), Range(5f, 20f)]
+    public float DespawnTime = 5f;
+
+    private Vector3 shotDir;
+
+    private Rigidbody rb;
+
+    private float timeActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+
+        // This is transform.up because the shot is rotated 90 degrees on X axis. Therefore, subject to change.
+        shotDir = this.transform.up;
+
+        timeActive = 0f;
+
+        // Detach this gameobject from the ShotTransform
+        this.gameObject.transform.parent = null; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = shotDir * ShotSpeed;
+
+        // Keep track of the time this shot has been active in the scene.
+        timeActive += Time.deltaTime;
+
+        // Deactivate this shot if it has been around for DespawnTime seconds.
+        if (timeActive >= DespawnTime)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Destroy an enemy that has been collided with.
+        if (other.tag == "Enemy" || other.tag == "enemy")
+        {
+            // TODO: Lessen enemy's health and only destroy if health <= 0
+            other.gameObject.SetActive(false);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
