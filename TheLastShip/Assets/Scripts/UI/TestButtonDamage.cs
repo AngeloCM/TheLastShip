8fea6120b3492ccb80c705618b93b8f755f61e17
Luke Uritescu <Luritescu@gmail.com>
2020-02-06 15:13:23 -0600

    UI Health, Progress, Thruster bars
    Progress and Health bars function as I intended, thruster is almost ready.
    
    Each bar has a UI Canvas in the prefab.
    
    I createed two different test scenes. One is for the Slider health bar which I personally was not a fan of. The other is the FillerHealthTest which uses the Filled image type for the UI.
    
    The buttons for each of the scene just are a way to show how the bars looks when they fill/empty.

---
+++ TheLastShip/Assets/Scripts/UI/TestButtonDamage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TestButtonDamage : MonoBehaviour
+{
+    public HealthSlider slider;
+    public HealthBar filler;
+    public ThrusterBarScript thruster;
+    public ProgressBarScript progress;
+    void Awake()
+    {
+
+    }
+    public void DealDamage()
+    {
+        slider.TakeDamage(15f);
+    }
+    public void FillerDealDamage()
+    {
+        filler.Damage(17);
+        progress.IncreaseProgress(10);
+        
+    }
+}
