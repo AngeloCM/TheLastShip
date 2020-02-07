1dfe26635af25f368e4131cd959d97e61ff02fab
luke-goldfain <awsumluke22@gmail.com>
2020-02-06 22:41:44 -0600

    Added PlayerShipMovement.cs, tweaked projectile speeds, added state enums to PlayerController.cs
    - The script PlayerShipMovement.cs moves the player ship around in the view, giving visual feedback to turning and thrust.
    - Enums added to PlayerController.cs keep track of rotation direction and acceleration, providing context to PlayerShipMovement.cs.
    - Primary projectile speed increased to 500 from 200.
    - Secondary projectile speed increased to 250 from 150.

--- TheLastShip/Assets/Scripts/Player/PlayerShipMovement.cs
+++ TheLastShip/Assets/Scripts/Player/PlayerShipMovement.cs
@@ -11,12 +11,36 @@ public class PlayerShipMovement : MonoBehaviour
     [SerializeField, Tooltip("The player ship model.")]
     public GameObject PlayerShipModel;
 
+    [SerializeField, Tooltip("The camera GameObject.")]
+    public GameObject CameraGameObject;
+
+    [SerializeField, Tooltip("The position of the ship model when moving straight.")]
+    private Vector3 modelPosStraight;
+    [SerializeField, Tooltip("The position of the ship model when turning upwards.")]
+    private Vector3 modelPosBottom;
+    [SerializeField, Tooltip("The position of the ship model when turning downwards.")]
+    private Vector3 modelPosTop;
+    [SerializeField, Tooltip("The position of the ship model when turning right.")]
+    private Vector3 modelPosLeft;
+    [SerializeField, Tooltip("The position of the ship model when turning left.")]
+    private Vector3 modelPosRight;
+
+    [SerializeField, Tooltip("The position of the camera when maintaining a constant speed.")]
+    private Vector3 cameraPosCoasting;
+    [SerializeField, Tooltip("The position of the camera when accelerating.")]
+    private Vector3 cameraPosAccelerating;
+    [SerializeField, Tooltip("The position of the camera when decelerating.")]
+    private Vector3 cameraPosDecelerating;
+
     private PlayerController pCont;
 
-    [HideInInspector]
-    public Vector3 PlayerShipTargetLocation;
+    private PlayerController.TurnDirection currentTurnDir;
 
-    private Vector3 prevRotation; // The rotation of the ship on the previous frame
+    private PlayerController.AccelerationState currentAccel;
+
+    private Vector3 PlayerShipTargetLocation;
+
+    private Vector3 CameraTargetLocation;
 
     // Start is called before the first frame update
     void Start()
@@ -27,14 +51,56 @@ public class PlayerShipMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.transform.position = Vector3.Lerp(this.gameObject.transform.position, PlayerShipTargetLocation, 0.4f);
+        currentTurnDir = pCont.CurrentTurnDirection; // Update turn direction
 
-        if (this.gameObject.transform.rotation.eulerAngles.y < prevRotation.y)
+        currentAccel = pCont.CurrentAcceleration; // Update acceleration
+
+        UpdatePlayerShipLocation();
+
+        UpdateCameraLocation();
+    }
+
+    private void UpdatePlayerShipLocation()
+    {
+        if (!GameSettings.IsPaused)
+            PlayerShipModel.transform.localPosition = Vector3.Slerp(PlayerShipModel.transform.localPosition, PlayerShipTargetLocation, 0.02f);
+
+        switch (currentTurnDir)
         {
-
+            case PlayerController.TurnDirection.down:
+                PlayerShipTargetLocation = modelPosTop;
+                break;
+            case PlayerController.TurnDirection.up:
+                PlayerShipTargetLocation = modelPosBottom;
+                break;
+            case PlayerController.TurnDirection.right:
+                PlayerShipTargetLocation = modelPosLeft;
+                break;
+            case PlayerController.TurnDirection.left:
+                PlayerShipTargetLocation = modelPosRight;
+                break;
+            case PlayerController.TurnDirection.none:
+                PlayerShipTargetLocation = modelPosStraight;
+                break;
         }
+    }
 
-        // Set previous rotation at the end of the current step.
-        prevRotation = this.gameObject.transform.rotation.eulerAngles;
+    private void UpdateCameraLocation()
+    {
+        if (!GameSettings.IsPaused)
+            CameraGameObject.transform.localPosition = Vector3.Lerp(CameraGameObject.transform.localPosition, CameraTargetLocation, 0.08f);
+
+        switch (currentAccel)
+        {
+            case PlayerController.AccelerationState.accelerating:
+                CameraTargetLocation = cameraPosAccelerating;
+                break;
+            case PlayerController.AccelerationState.coasting:
+                CameraTargetLocation = cameraPosCoasting;
+                break;
+            case PlayerController.AccelerationState.decelerating:
+                CameraTargetLocation = cameraPosDecelerating;
+                break;
+        }
     }
 }
