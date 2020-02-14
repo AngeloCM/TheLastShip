3aa835849eb8969a58fc8bd4e2b921344dd3198d
Luke Uritescu <Luritescu@gmail.com>
2020-02-13 19:23:27 -0600

    Ship  Progress Icon
    The Ship Progress Icon follows the bar, I just clamped a slider to the progress bar. Should be fine for what we wanted. The only thing is designers can mess with the size and placement of thee bars. THey don't fit the screen as well as they probably should

---
+++ TheLastShip/Assets/Scripts/UI/GameSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameSettings
+{
+    public enum ControlScheme
+    {
+        classic,
+        frontline,
+        frontlineBeta,
+    }
+
+    public static ControlScheme CurrentControlScheme = ControlScheme.classic;
+
+    public static bool InvertYLook;
+
+    public static bool IsPaused;
+}
